943bd500ad6b6aaef504c1c58bc884aee76feff9
deepspacediver <luka.koscik@gmail.com>
2023-01-23 08:10:09 +0100

    add firebase

---
+++ src/firebase/firebase-config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCBNI7nZIoSJtVbMARKhYHpZjHQ9CJcV4I",
+  authDomain: "wheres-waldo-7e128.firebaseapp.com",
+  projectId: "wheres-waldo-7e128",
+  storageBucket: "wheres-waldo-7e128.appspot.com",
+  messagingSenderId: "504744157634",
+  appId: "1:504744157634:web:f4251abfce9d283079f65b",
+  measurementId: "G-XRTNCL4C1N",
+};
+
+export default firebaseConfig;
