a13f5e1db2dc81d5d07c06e92dcf8a866e591984
deepspacediver <luka.koscik@gmail.com>
2023-01-24 14:08:45 +0100

    refactor TargetinBox location handling

--- src/components/TaggingHandler/TaggingHandler.tsx
+++ src/components/TaggingHandler/TaggingHandler.tsx
@@ -6,74 +6,25 @@ import hotlineBackground from "../../assets/images/main-image/hotline-miami-imag
 import "./TaggingHandler.styles.css";
 
 const TaggingHandler = () => {
-  const [tagLocation, setTagLocation] = useState({
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  });
   const [isActive, setIsActive] = useState(false);
   const [cursorCoords, setCursorCoords] = useState({
-    x: 0,
-    y: 0,
+    left: 0,
+    top: 0,
   });
-  const dropdownRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (!isActive) return;
+  const handleTag = (e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+    if (target.classList.contains("btn")) return;
 
-    const getTargetLocation = () => {
-      const targetingBoxRect = dropdownRef.current?.getBoundingClientRect();
-      const parentRect =
-        dropdownRef.current?.parentElement?.getBoundingClientRect();
-
-      if (!targetingBoxRect || !parentRect) return;
-      const { x, y } = cursorCoords;
-      const calculatedLeft = x - targetingBoxRect.width / 2;
-      const calculatedTop = y - targetingBoxRect.height / 2;
-
-      const topCoord =
-        ((calculatedTop - parentRect.top) / parentRect.height) * 100;
-      const leftCoord =
-        ((calculatedLeft - parentRect.left) / parentRect.width) * 100;
-      const bottomCoord =
-        ((calculatedTop - parentRect.top + targetingBoxRect.height) /
-          parentRect.height) *
-        100;
-      const rightCoord =
-        ((calculatedLeft - parentRect.left + targetingBoxRect.width) /
-          parentRect.width) *
-        100;
-
-      setTagLocation({
-        top: topCoord,
-        left: leftCoord,
-        bottom: bottomCoord,
-        right: rightCoord,
-      });
-    };
-
-    getTargetLocation();
-  }, [isActive, cursorCoords]);
-
-  const handleTag = (e: MouseEvent<HTMLDivElement>) => {
     setIsActive((prevState) => !prevState);
-    if (isActive) {
-      setTagLocation({
-        top: -9999999,
-        left: 0,
-        bottom: 0,
-        right: 0,
-      });
-      return;
-    }
+    console.log(e.clientX, e.clientY);
     setCursorCoords({
-      x: e.clientX,
-      y: e.clientY,
+      left: e.clientX,
+      top: e.clientY,
     });
   };
 
-  const { top, left } = tagLocation;
+  const { left, top } = cursorCoords;
   return (
     <>
       <div className="image-wrapper" onClick={handleTag}>
@@ -82,12 +33,7 @@ const TaggingHandler = () => {
           alt="hotline miami characters"
           name="main-background"
         />
-        <TargetingBox
-          ref={dropdownRef}
-          left={left}
-          top={top}
-          isActive={isActive}
-        />
+        <TargetingBox left={left} top={top} isActive={isActive} />
       </div>
     </>
   );
--- src/components/TargetingBox/TargetingBox.tsx
+++ src/components/TargetingBox/TargetingBox.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from "react";
+import { useRef } from "react";
 import "./TargetingBox.styles.css";
 
 export interface TargetingBoxProps {
@@ -9,20 +9,59 @@ export interface TargetingBoxProps {
 
 export type CharacterRef = HTMLDivElement;
 
-const TargetingBox = forwardRef<CharacterRef, TargetingBoxProps>(
-  ({ left, top, isActive }, ref) => {
-    return (
-      <>
-        {isActive && (
-          <div
-            className="character-dropdown"
-            style={{ left: `${left}%`, top: `${top}%` }}
-            ref={ref}
-          ></div>
-        )}
-      </>
-    );
-  }
-);
+const TargetingBox = ({ left, top, isActive }: TargetingBoxProps) => {
+  const targetingBoxRef = useRef<HTMLDivElement>(null);
+
+  const getTargetLocation = () => {
+    const targetingBoxRect = targetingBoxRef.current?.getBoundingClientRect();
+    const parentRect =
+      targetingBoxRef.current?.parentElement?.getBoundingClientRect();
+
+    if (!targetingBoxRect || !parentRect) return;
+    const calculatedLeft = left - targetingBoxRect.width / 2;
+    console.log({ calculatedLeft });
+    const calculatedTop = top - targetingBoxRect.height / 2;
+
+    const topCoord =
+      ((calculatedTop - parentRect.top) / parentRect.height) * 100;
+    const leftCoord =
+      ((calculatedLeft - parentRect.left) / parentRect.width) * 100;
+    console.log({ leftCoord });
+    const bottomCoord =
+      ((calculatedTop - parentRect.top + targetingBoxRect.height) /
+        parentRect.height) *
+      100;
+    const rightCoord =
+      ((calculatedLeft - parentRect.left + targetingBoxRect.width) /
+        parentRect.width) *
+      100;
+
+    console.log({ topCoord, leftCoord, bottomCoord, rightCoord });
+  };
+
+  return (
+    <>
+      {isActive && (
+        <div
+          className="targeting-box"
+          style={{ left: `${left}px`, top: `${top}px` }}
+          ref={targetingBoxRef}
+        >
+          <ul className="targeting-box_list" onClick={getTargetLocation}>
+            <li>
+              <button className="btn">Helmet</button>{" "}
+            </li>
+            <li>
+              <button className="btn">Rasmus</button>
+            </li>
+            <li>
+              <button className="btn">Richard</button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default TargetingBox;
