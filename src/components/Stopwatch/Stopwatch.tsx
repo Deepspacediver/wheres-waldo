4a06d4f38d5be055835c8e3731ad887b14088dff
deepspacediver <luka.koscik@gmail.com>
2023-01-30 19:10:27 +0100

    update Stopwatch activates when game start

--- src/components/Stopwatch/Stopwatch.tsx
+++ src/components/Stopwatch/Stopwatch.tsx
@@ -1,29 +1,37 @@
 import { useState, useRef, useEffect, memo, MouseEvent } from "react";
+import Overlay from "../Overlay/Overlay";
+import SubmitScore from "../SubmitScore/SubmitScore";
+import Scoreboard from "../Scoreboard/Scoreboard";
 import "./Stopwatch.styles.css";
 
 type Timer = ReturnType<typeof setInterval>;
 interface StopwatchProps {
   isGameOver: () => boolean;
+  canStartGame: boolean;
 }
 
-const Stopwatch = memo(({ isGameOver }: StopwatchProps) => {
+const Stopwatch = memo(({ isGameOver, canStartGame }: StopwatchProps) => {
   const [startTime, setStartTime] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
+  const [isScoreSubmit, setIsScoreSubmit] = useState(false);
   const intervalRef = useRef<Timer | null>(null);
   console.log("stopwatch");
+
+  useEffect(() => {
+    const startStopwatch = () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+      setStartTime(Date.now());
+      setCurrentTime(Date.now());
+      intervalRef.current = setInterval(() => setCurrentTime(Date.now()), 10);
+    };
+    if (canStartGame) startStopwatch();
+  }, [canStartGame]);
 
   useEffect(() => {
     if (isGameOver() && intervalRef.current) {
       clearInterval(intervalRef.current);
     }
   }, [isGameOver]);
-
-  const startStopwatch = (e: MouseEvent<HTMLButtonElement>) => {
-    if (intervalRef.current) clearInterval(intervalRef.current);
-    setStartTime(Date.now());
-    setCurrentTime(Date.now());
-    intervalRef.current = setInterval(() => setCurrentTime(Date.now()), 10);
-  };
 
   let timePassed = 0;
   let minutes = 0;
@@ -37,12 +45,22 @@ const Stopwatch = memo(({ isGameOver }: StopwatchProps) => {
 
   return (
     <div>
-      <div>{timePassed}</div>
       <div>
         {minutes < 10 ? "0" + minutes : minutes}:
         {seconds < 10 ? "0" + seconds : seconds}
       </div>
-      <button onClick={startStopwatch}> Start</button>
+      {isGameOver() && (
+        <Overlay>
+          {!isScoreSubmit ? (
+            <SubmitScore
+              time={timePassed}
+              setIsScoreSubmit={setIsScoreSubmit}
+            />
+          ) : (
+            <Scoreboard />
+          )}
+        </Overlay>
+      )}
     </div>
   );
 });
