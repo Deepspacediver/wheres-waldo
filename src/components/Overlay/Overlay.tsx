686e4e71e400cff5a4611be93a3d14c6a7a0746a
deepspacediver <luka.koscik@gmail.com>
2023-02-09 08:30:30 +0100

    add styles & forwardRef to Overlay for Transition

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,10 +1,17 @@
-import "./Overlay.styles.css";
+import { forwardRef } from "react";
 import { ChildrenAsProps } from "../../common/types";
+import "./Overlay.styles.css";
 
 interface OverlayProps extends ChildrenAsProps {}
 
-const Overlay = ({ children }: OverlayProps) => {
-  return <div className="overlay">{children}</div>;
-};
+const Overlay = forwardRef<HTMLDivElement, OverlayProps>(
+  ({ children }, ref) => {
+    return (
+      <div ref={ref} className="overlay">
+        <div className="overlay__modal-wrapper">{children}</div>
+      </div>
+    );
+  }
+);
 
 export default Overlay;
