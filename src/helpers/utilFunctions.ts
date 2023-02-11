e3481d746353c0cd6fa3d5fd96920e7b1e8d2cad
deepspacediver <luka.koscik@gmail.com>
2023-02-11 13:06:33 +0100

    add convertTime helper

--- src/helpers/utilFunctions.ts
+++ src/helpers/utilFunctions.ts
@@ -1,3 +1,12 @@
 const capitalize = (x: string) => x[0].toUpperCase() + x.slice(1);
 
-export { capitalize };
+const convertTime = (time: number) => {
+  const minutes = Math.trunc(time / 60);
+  const seconds = Math.trunc(time % 60);
+  return {
+    minutes: minutes < 10 ? String("0" + minutes) : String(minutes),
+    seconds: seconds < 10 ? String("0" + seconds) : String(seconds),
+  };
+};
+
+export { capitalize, convertTime };
